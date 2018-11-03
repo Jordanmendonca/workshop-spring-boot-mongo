734a83b373f65cb450e10f94d3f56fb39093477a
Jordan Mendonça <jordan_mendonca@hotmail.comclearclsclscleargit cleargit config --global user.name Jordan>
2018-11-03 12:21:24 -0300

    Criando Entity com Post alinhado

---
+++ src/main/java/com/jordanmendonca/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.jordanmendonca.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jordanmendonca.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+	
+			
+}
